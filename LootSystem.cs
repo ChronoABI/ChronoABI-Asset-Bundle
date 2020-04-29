e5e6e931fd07228e244e88a23cd0b1f1fa7ef2de
ChronoABI <56946671+ChronoABI@users.noreply.github.com>
2020-04-29 10:31:51 +0545

    First Script

---
+++ LootSystem.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class ItemToSpawn
+{
+    public GameObject item;
+    public float spawnRate;
+    [HideInInspector] public float minSpawnProb, maxSpawnProb;
+}
+
+public class LootSystem : MonoBehaviour
+{
+    public ItemToSpawn[] itemToSpawn;
+    
+    void Start()
+    {
+
+        for (int i = 0; i < itemToSpawn.Length; i++)
+        {
+
+            if (i == 0)
+            {
+                itemToSpawn[i].minSpawnProb = 0;
+                itemToSpawn[i].maxSpawnProb = itemToSpawn[i].spawnRate - 1;
+            }
+            else
+            {
+                itemToSpawn[i].minSpawnProb = itemToSpawn[i - 1].maxSpawnProb + 1;
+                itemToSpawn[i].maxSpawnProb = itemToSpawn[i].minSpawnProb + itemToSpawn[i].spawnRate - 1;
+            }
+        }
+        Spawnner();
+        
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Spawnner();
+        }
+    }
+
+   void Spawnner()
+    {
+        float randomNum = Random.Range(0, 100);//56
+
+        for (int i = 0; i < itemToSpawn.Length; i++)
+        {
+            if(randomNum>=itemToSpawn[i].minSpawnProb && randomNum<= itemToSpawn[i].maxSpawnProb)
+            {
+                
+
+                Instantiate(itemToSpawn[i].item, transform.position, Quaternion.identity);
+                break;
+            }
+        }
+    }
+    
+}
